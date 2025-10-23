cfebb36b652b8e0e8dbdb16117f662ddf2df92f1
Boss <khunboss2252@gmail.com>
2025-10-24 00:18:16 +0700

    Sticky Navhead

---
+++ JS-component/shrink-nav.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('header.navbar');
+  if (window.scrollY > 50) {           // เมื่อ scroll เกิน 50px
+    navbar.classList.add('shrink');
+  } else {
+    navbar.classList.remove('shrink');
+  }
+});
